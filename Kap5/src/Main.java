fde0de995cd8919d694e9c508ef57c0ca9128477
Mezzeto <Mathias_Lundby@hotmail.com>
2018-10-17 14:11:33 +0200

    ignoreIml
    jejeje

---
+++ Kap5/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("kaker");
+        sut();
+        System.out.println("Endret  ");
+    }
+
+    public static void sut() {
+        System.out.println("sut");
+    }
+}
